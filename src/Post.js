3bd40fba5a7f8bcc456ea3f04b04dab916b28e04
Michael McCann <michaelmccann@Michaels-Air.station>
2022-01-09 16:56:02 +0000

    Added in the logic to make the comments visible on the app.

--- src/Post.js
+++ src/Post.js
@@ -48,6 +48,15 @@ function Post({postId, username, caption, imageUrl}) {
             </img>
 
             <h4 className='post__text'><strong>{username}</strong> {caption}</h4>
+            
+            <div className="post__comments">
+                {comments.map((comment) => (
+                    <p>
+                    <strong>{comment.username}</strong> {comment.text}
+                    </p>
+                ))}
+            </div>
+
             <form className='post__commentBox'>
                 <input
                     className='post__input'
