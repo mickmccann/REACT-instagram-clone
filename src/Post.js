44e0a6279bd714cc84560968712c2b6bf7676d19
Michael McCann <michaelmccann@MICHAELS-AIR.station>
2022-01-13 16:10:45 +0000

    Put the comment box inside {user &&} to hide the comment box when logged out.

--- src/Post.js
+++ src/Post.js
@@ -63,8 +63,9 @@ function Post({postId, user, username, caption, imageUrl}) {
                     </p>
                 ))}
             </div>
-
-            <form className='post__commentBox'>
+                    
+            {user && (
+                <form className='post__commentBox'>
                 <input
                     className='post__input'
                     type='text'
@@ -81,6 +82,8 @@ function Post({postId, user, username, caption, imageUrl}) {
                     Post
                 </button>
            </form>
+            )}
+            
         </div>
     )
 }
